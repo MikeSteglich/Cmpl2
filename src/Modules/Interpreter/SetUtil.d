ba9359f32d6402d030b8ea44e323e9391f91039a
Thomas Schleiff <t.schleiff@gmail.com>
2019-04-14 12:11:26 +0200

    tuple matching; several corrections

--- src/Modules/Interpreter/SetUtil.d
+++ src/Modules/Interpreter/SetUtil.d
@@ -1,5 +1,4 @@
 SetUtil.o SetUtil.d : SetUtil.cc SetUtil.hh ../../CommonData/CmplVal.hh \
-<<<<<<< HEAD
  ../../CommonData/Tuple.hh ../../CommonData/CmplVal.hh \
  ../../CommonData/SetBase.hh ../../CommonData/Tuple.hh SetInfinite.hh \
  TupleUtil.hh Interval.hh Interpreter.hh ../../Control/ModuleBase.hh \
@@ -17,18 +16,3 @@ SetUtil.o SetUtil.d : SetUtil.cc SetUtil.hh ../../CommonData/CmplVal.hh \
  ../../CommonData/CmplArray.hh StackValue.hh CodeBlockContext.hh \
  ../../Control/MainControl.hh ../../Control/ThreadHandler.hh \
  ../../Control/FileBase.hh ../../Control/ExtensionBase.hh
-=======
-  ../../CommonData/Tuple.hh ../../CommonData/SetBase.hh TupleUtil.hh \
-  Interval.hh Interpreter.hh ../../Control/ModuleBase.hh \
-  ../../Control/CmdLineOptList.hh ../../Control/LocationInfo.hh \
-  ../../Control/MainData.hh ../../Control/StringStore.hh \
-  ../../Control/ErrorHandler.hh ../../Control/ThreadHandler.hh \
-  ../../CommonData/SyntaxElements.hh \
-  ../../CommonData/../Control/MainData.hh ../../CommonData/IntCode.hh \
-  ../../CommonData/OptModel.hh ../../CommonData/ValueTree.hh \
-  ../../CommonData/CmplArray.hh ../../CommonData/SetIterator.hh \
-  ../../CommonData/../Control/StringStore.hh \
-  ../../CommonData/ExternDataInfo.hh ExecContext.hh ValueStore.hh \
-  StackValue.hh CodeBlockContext.hh ../../Control/MainControl.hh \
-  ../../Control/FileBase.hh ../../Control/ExtensionBase.hh
->>>>>>> 1f807a82ae635c305dd72280be747894694aea1a
